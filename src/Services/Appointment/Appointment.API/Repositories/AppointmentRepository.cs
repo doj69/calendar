7fe334ceef9ed09720e16d1b4d6f4e3a968c429c
Doni Junior <doni.junior.@gmail.com>
2021-08-16 10:23:58 +0700

    developing business layer with repository pattern

---
+++ src/Services/Appointment/Appointment.API/Repositories/AppointmentRepository.cs
@@ -0,0 +1,62 @@
+﻿using Appointment.API.Data;
+using Appointment.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Appointment.API.Repositories
+{
+    public class AppointmentRepository : IAppointmentRepository
+    {
+        private readonly IAppointmentContext _context;
+        private readonly FilterDefinitionBuilder<AppointmentEntity> filterDefinitionBuilder = Builders<AppointmentEntity>.Filter;
+
+        public AppointmentRepository(IAppointmentContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        public async Task CreateAppointment(AppointmentEntity appointment)
+        {
+            await _context.Appointments.InsertOneAsync(appointment);
+        }
+
+        public async Task<bool> DeleteAppointment(string id)
+        {
+            FilterDefinition<AppointmentEntity> filter = filterDefinitionBuilder.Eq(x => x.Id, id);
+            DeleteResult deleteResult = await _context.Appointments.DeleteOneAsync(filter);
+
+            return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+        }
+
+        public async Task<AppointmentEntity> GetAppointment(string id)
+        {
+            var result = await _context.Appointments.Find(x => x.Id.Equals(id)).FirstOrDefaultAsync();
+            return result;
+        }
+
+        public async Task<IEnumerable<AppointmentEntity>> GetAppointments()
+        {
+            var result = await _context.Appointments.Find(x => true).ToListAsync();
+            return result;
+        }
+
+        public async Task<bool> IsConflictAppointment(AppointmentEntity appointment)
+        {
+            var result = await _context.Appointments.Find(filter: x => appointment.End < x.End && x.Start < appointment.End).ToListAsync();
+            return result.Count > 0;
+        }
+
+        public async Task<bool> IsConflictAppointmentUpdate(AppointmentEntity appointment)
+        {
+            var result = await _context.Appointments.Find(filter: x => appointment.End < x.End && x.Start < appointment.End && x.Id != appointment.Id).ToListAsync();
+            return result.Count > 0;
+        }
+
+        public async Task<bool> UpdateAppointment(AppointmentEntity appointment)
+        {
+            var updateResult = await _context.Appointments.ReplaceOneAsync(filter: x => x.Id.Equals(appointment.Id), replacement: appointment);
+            return updateResult.IsAcknowledged && updateResult.ModifiedCount > 0;
+        }
+    }
+}
---
+++ src/Services/Appointment/Appointment.API/Repositories/IAppointmentRepository.cs
@@ -0,0 +1,17 @@
+﻿using Appointment.API.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Appointment.API.Repositories
+{
+    public interface IAppointmentRepository
+    {
+        Task<IEnumerable<AppointmentEntity>> GetAppointments();
+        Task<AppointmentEntity> GetAppointment(string id);        
+        Task CreateAppointment(AppointmentEntity appointment);
+        Task<bool> UpdateAppointment(AppointmentEntity appointment);
+        Task<bool> DeleteAppointment(string id);
+        Task<bool> IsConflictAppointment(AppointmentEntity appointment);
+        Task<bool> IsConflictAppointmentUpdate(AppointmentEntity appointment);
+    }
+}
