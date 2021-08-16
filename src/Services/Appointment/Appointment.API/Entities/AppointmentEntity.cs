9cc442f516de0f93552d13e639c3d96a77ae62fb
Doni Junior <doni.junior.@gmail.com>
2021-08-16 10:16:20 +0700

    create entities

---
+++ src/Services/Appointment/Appointment.API/Entities/AppointmentEntity.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace Appointment.API.Entities
+{
+    public class AppointmentEntity
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonElement("Name")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [BsonDateTimeOptions(Kind = DateTimeKind.Unspecified)]
+        public DateTime Start { get; set; }
+        [BsonDateTimeOptions(Kind = DateTimeKind.Unspecified)]
+        public DateTime End { get; set; }
+        public string Color { get; set; }
+        public bool Timed { get; set; }
+    }
+}
